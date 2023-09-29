1583521b95a75ccc37fa5e1a3117801b0cdb837f
Iamneo <techadmin@iamneo.ai>
2023-09-29 06:50:35 +0000

    Backup Commit

---
+++ ComplaintManagementSystem/Controllers/ComplaintController.cs
@@ -0,0 +1,22 @@
+﻿using ComplaintManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ComplaintManagementSystem.Models; // Import your model classes
+
+public class ComplaintController : Controller
+{
+    private readonly ComplaintDbContext _context; // Replace with your DbContext class
+
+    public ComplaintController(ComplaintDbContext context)
+    {
+        _context = context;
+    }
+    // Write your Action Methods here...
+    // Submit() - Action to display the form for customers to submit a complaint
+    // Submit(Complaint complaint) - POST action to handle complaint submission
+    // Dashboard() - Action to display a list of complaints for the logged-in representative
+    // UpdateStatus(int id, string status) - Action to update the status of a complaint
+}
---
+++ ComplaintManagementSystem/Controllers/CustomerServiceRepresentativeController.cs
@@ -0,0 +1,20 @@
+﻿using ComplaintManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class CustomerServiceRepresentativeController : Controller
+{
+    private readonly ComplaintDbContext _context; // Replace with your DbContext class
+
+    public CustomerServiceRepresentativeController(ComplaintDbContext context)
+    {
+        _context = context;
+    }
+    // Write your Action Methods here...
+    // Task<IActionResult> Index() - Action to view and manage representative information
+    // Task<IActionResult> Edit(int id) - Action to edit representative information
+    // Task<IActionResult> Edit(int id, CustomerServiceRepresentative representative) - Action to edit representative information
+}
